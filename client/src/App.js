b3900e29c6c2acd051085bb13fcbcdef9e9e5499
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-11-08 18:49:06 -0500

    Integrated router-added Link to landing and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,23 @@ import './App.css';
 import React , { Fragment} from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 const App = () => (
-  <Fragment>
-    <Navbar/>
-    <Landing/>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar/>
+      <Route exact path='/' component={Landing}/>
+      {/* //we want to have section with class name container because every page has container class around it except landing page. */}
+      <section className="container">
+        <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
