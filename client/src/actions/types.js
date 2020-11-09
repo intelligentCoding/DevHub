29df3983906e8d329b9a9671f3bc19dff0a84b6d
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-11-09 10:24:33 -0500

    added profile actioins and reducers

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,8 @@
-
+//Alerts types
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+//Registeration Types
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const USER_LOADED = 'USER_LOADED';
@@ -8,3 +10,7 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
+
+//Profile types
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
