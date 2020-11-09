7162951573e2498f8a1eec5a1849ef9f9320d8c5
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-11-09 14:30:22 -0500

    create profile option / spinner added to Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,46 @@
-import React , { useEffect }from 'react'
+import React , { useEffect, Fragment }from 'react'
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {getCurrentProfile} from '../../actions/profile';
 import profile from '../../reducers/profile';
-const Dashboard = ({getCurrentProfile, auth, profile}) => {
+import { Link } from 'react-router-dom';
+//spinner
+import Spinner from '../layout/spinner';
+const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}}) => {
     useEffect(() => {
         getCurrentProfile();
     }, [getCurrentProfile])
     return (
-        <div>   
-            Dashboard
-        </div>
-    )
+        <Fragment>
+          <h1 className="large text-primary">Dashboard</h1>
+          <p className="lead">
+            <i className="fas fa-user" /> Welcome {user && user.name}
+          </p>
+          {profile !== null ? (
+            <Fragment>
+              {/* <DashboardActions />
+              <Experience experience={profile.experience} />
+              <Education education={profile.education} /> */}
+    
+              <div className="my-2">
+                <button className="btn btn-danger" 
+                // onClick={() => deleteAccount()}
+                >
+                  <i className="fas fa-user-minus" /> Delete My Account
+                </button>
+              </div>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <p>You have not yet setup a profile, please add some info</p>
+              <Link to="/create-profile" className="btn btn-primary my-1">
+                Create Profile
+              </Link>
+            </Fragment>
+          )}
+        </Fragment>
+      );
+    
 }
 
 Dashboard.propTypes = {
