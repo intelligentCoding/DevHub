840e2e1acf48f307dda724ff5591ecfdc9e0d726
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-11-09 20:28:07 -0500

    display profile reducers and actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,7 +7,9 @@ import {
     PROFILE_ERROR,
     UPDATE_PROFILE,
     ACCOUNT_DELETED,
-    CLEAR_PROFILE
+    CLEAR_PROFILE,
+    GET_PROFILES,
+    GET_REPOS,
 } from './types';
 
 //Get signed in user's profile
@@ -212,3 +214,60 @@ export const deleteEducation = (id) => async (dispatch) => {
       });
     }
   };
+
+  //get all the profiles
+export const getProfiles = () => async (dispatch) => {
+    //from preventing of flashing of previous profile
+    dispatch({ type: CLEAR_PROFILE });
+  
+    try {
+      const res = await axios.get('/api/profile');
+  
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+
+//profile by id
+export const getProfileById = (userId) => async (dispatch) => {
+    try {
+      const res = await axios.get(`/api/profile/user/${userId}`);
+  
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+
+//get github repos
+// Get Github repos
+export const getGithubRepos = (username) => async (dispatch) => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`);
+
+        dispatch({
+        type: GET_REPOS,
+        payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+        type: NO_REPOS
+        });
+    }
+};
+
+
+
