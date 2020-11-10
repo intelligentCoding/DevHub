34375fb54decb380eb93ef7b32ff83f3f059ed0c
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2020-11-09 23:07:00 -0500

    ready for deployment to heroku

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-
-//connect database
+const path = require('path');
+//connect databasconste
 connectDB();
-//Just testing if the express server is running.
-app.get("/", (req, res) => res.send("API running"));
+
 
 //We have to initilize the middleware for body parser
 app.use(express.json({extended: false}))
@@ -15,6 +14,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/post"));
+
+//we need to serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 5000;
 
